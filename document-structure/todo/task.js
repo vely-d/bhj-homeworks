3a8b448f8338ac0ef4df5d5933971de0d996be84
vely1012 <damyanovv228666@gmail.com>
2025-05-11 15:36:37 +0300

    document-structure: further improvements

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,54 +1,34 @@
-// function test() {
-//     window.localStorage.setItem("tasks", "1,2,3,4,5,6");
-// }
-
-// test();
-
-
-
-
 const tasksListEl = document.querySelector("#tasks__list");
 const taskInput = document.querySelector("#task__input");
 
-let tasksList = window.localStorage.getItem("tasks").split(",");
-for(let t of tasksList) {
-    createTask(t);
-}
+void function() {
+    let localTasksItem = window.localStorage.getItem("tasks");
+    if(!localTasksItem) {
+        return;
+    }
+    for(let t of localTasksItem.split(",")) {
+        createTask(t);
+    }
+}();
 
 function updateLocalTasks() {
-    window.localStorage.setItem("tasks", String(tasksList));
+    window.localStorage.setItem("tasks", Array.from(tasksListEl.querySelectorAll(".task__title")).map(e => e.textContent));
 }
 
 function removeTask(event) {
-    let taskIndex = Array.prototype.indexOf.call(tasksListEl.children, this.parentElement);
-    tasksList.splice(taskIndex, 1);
     this.parentElement.remove();
     updateLocalTasks();
     event.preventDefault();
-
-    console.log(tasksList);
-    console.log(window.localStorage.getItem("tasks"));
 }
 
 function createTask(taskText) {
-    let taskEl = document.createElement("div");
-    taskEl.classList.add("task");
-    // taskEl.dataset.id = tasksListEl.children.length;
-
-    let taskTitle = document.createElement("div");
-    taskTitle.classList.add("task__title");
-    taskTitle.textContent = taskText;
-
-    let taskRemove = document.createElement("a");
-    taskRemove.classList.add("task__remove");
-    taskRemove.href = "#";
-    taskRemove.addEventListener("click", removeTask);
-    taskRemove.textContent = "×";
-
-    taskEl.appendChild(taskTitle);
-    taskEl.appendChild(taskRemove);
-
-    tasksListEl.appendChild(taskEl);
+    tasksListEl.insertAdjacentHTML("beforeend", `
+        <div class="task">
+            <div class="task__title">${taskText}</div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>
+    `);
+    tasksListEl.lastElementChild.querySelector(".task__remove").addEventListener("click", removeTask);
 }
 
 document.querySelector(".tasks__add").addEventListener("click", function(event) {
@@ -58,6 +38,5 @@ document.querySelector(".tasks__add").addEventListener("click", function(event) 
         return;
     }
     createTask(taskInput.value);
-    tasksList.push(taskInput.value);
     updateLocalTasks();
 });
