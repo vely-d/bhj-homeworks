e8dcb8d6595f67ca605b9ba27af89f9f12456072
vely1012 <damyanovv228666@gmail.com>
2025-05-11 05:03:44 +0300

    progress bar now actually tracks upload

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,20 +1,16 @@
 const progressBar = document.querySelector("#progress");
 
 let xhr = new XMLHttpRequest();
-xhr.addEventListener("progress", function(event) {
-    progressBar.value = event.loaded / event.total;
 
-    // test
-    // progressBar.value = event.loaded / 2785799;
+xhr.upload.addEventListener("progress", function(event) {
+    progressBar.value = event.loaded / 1000281;
 });
 
 document.querySelector("#form").addEventListener("submit", function(event) {
     event.preventDefault();
 
     xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
-    xhr.send(new FormData(this));
-    
-    // test
-    // xhr.open("GET", "https://raw.githubusercontent.com/mdn/content/main/files/en-us/_wikihistory.json");
-    // xhr.send();
+    let data = new FormData(this);
+    data.append("digits", "1".repeat(1e6));
+    xhr.send(data);
 });
